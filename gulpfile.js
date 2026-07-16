e5d13cca11a0be6aa38f56d5a8a32b8dfd429a79
balaguru-k <144519978+balaguru-k@users.noreply.github.com>
2026-07-16 12:56:30 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var Paths = {
 };
 
 gulp.task('compile-scss', function() {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES, { allowEmpty: true })
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
